7ddcc420ba41c20229994cf7267b121ef6b9331c
AI-BNU-TEAMKG <ethanlu@mail.bnu.edu.cn>
2025-05-10 11:03:24 +0800

    Update GeneratorKnowledgeJson.py
    modify

--- MEduKGConstruction/GeneratorKnowledgeJson.py
+++ MEduKGConstruction/GeneratorKnowledgeJson.py
@@ -8,11 +8,11 @@ import json
 def readjson(filePath:str) -> list:
     raw_data = []
     with open(filePath, 'r', encoding='utf-8') as file:
-        # 使用 json.load() 解析 JSON 数据
+
         raw_data = json.load(file)
     return raw_data
-stard = readjson(r'F:\2.实验\3.Datasets\1.国家中小学智慧平台数据集\0.smartEdu\Course\json\biology\knowledgeBiology.json')
-charp = readjson(r'F:\2.实验\3.Datasets\1.国家中小学智慧平台数据集\0.smartEdu\Course\json\biology\biologyURL.json')
+stard = readjson(r')
+charp = readjson(r'')
 
 start = 0
 result = []
@@ -39,6 +39,6 @@ for key, value in stard.items():
     result.append(new)
     start += 1
     
-with open(r"F:\2.实验\3.Datasets\1.国家中小学智慧平台数据集\0.smartEdu\Course\json\biology\biologyKnowledge.json", 'w', encoding='utf-8') as merged_file:
+with open(r"n", 'w', encoding='utf-8') as merged_file:
     json.dump(result, merged_file, ensure_ascii=False, indent=4)
 
